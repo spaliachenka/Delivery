d114942d9f0437a9afa1afde74bf90a59918b283
Siarhei Paliachenka <paliachenka2@gmail.com>
2022-01-24 02:21:16 +0300

    editorconfig tuning

--- src/OrderService/FastMeal.OrderService.Domain/Models/DTO/OrderDetails.cs
+++ src/OrderService/FastMeal.OrderService.Domain/Models/DTO/OrderDetails.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FastMeal.OrderService.Domain.Models.DTO
+﻿namespace FastMeal.OrderService.Domain.Models.DTO
 {
     public record class OrderDetails(int ConsumerId, int RestaurantId, IEnumerable<OrderListItem> OrderListItems);
 }
